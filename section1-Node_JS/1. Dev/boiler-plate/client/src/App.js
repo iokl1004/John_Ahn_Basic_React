f6d2c3de2f2518e2c258f78b031e225db49ed9ce
iokl1004 <iokl3319@naver.com>
2024-09-12 12:59:48 +0900

    20240912
    32~34

--- section1-Node_JS/1. Dev/boiler-plate/client/src/App.js
+++ section1-Node_JS/1. Dev/boiler-plate/client/src/App.js
@@ -9,6 +9,7 @@ import Footer from "./components/views/Footer/Footer";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Auth from './hoc/auth'
 
 function App() {
   return (
@@ -16,9 +17,13 @@ function App() {
       <NavBar />
         <Router>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                {/* <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/register" element={<RegisterPage />} */}
+                
+                <Route path="/" element={Auth(LandingPage, null)} />
+                <Route path="/login" element={Auth(LoginPage, true)} />
+                <Route path="/register" element={Auth(RegisterPage, false)} />
             </Routes>
         </Router>
         <Footer />
--- section1-Node_JS/1. Dev/boiler-plate/client/src/_actions/user_action.js
+++ section1-Node_JS/1. Dev/boiler-plate/client/src/_actions/user_action.js
@@ -1,7 +1,8 @@
 import Axios from "axios";
 import {
       LOGIN_USER
-    , REGISTER_USER 
+    , REGISTER_USER
+    , AUTH_USER
 } from './types';
 
 export function loginUser(dataTosubmit) {
@@ -23,6 +24,18 @@ export function registerUser(dataTosubmit) {
 
     return {
         type : REGISTER_USER,
+        payload : request
+
+    }
+}
+
+export function auth(dataTosubmit) {
+    // 백엔드에서 가져온 데이터
+    const request = Axios.post('/api/users/auth')
+    .then(response => response.data )
+
+    return {
+        type : AUTH_USER,
         payload : request
 
     }
